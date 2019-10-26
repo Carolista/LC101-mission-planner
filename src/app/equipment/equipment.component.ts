49905b2c92f428f2c7a739e41d906ac76c98b414
Carolista <55961845+Carolista@users.noreply.github.com>
2019-10-25 22:25:42 -0500

    upgraded equipment section

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { DomElementSchemaRegistry } from '@angular/compiler';
 
 @Component({
-  selector: 'app-equipment',
-  templateUrl: './equipment.component.html',
-  styleUrls: ['./equipment.component.css']
+    selector: 'app-equipment',
+    templateUrl: './equipment.component.html',
+    styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
 
-  constructor() { }
+    pieceBeingEdited: string = null;
 
-  ngOnInit() {
-  }
+    equipment: string[] = ["sonic screwdriver", "hair spray", "coat"]; 
 
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    addEquip(equipName: string) {
+        for (let c = 0; c < this.equipment.length; c++) {
+            if (this.equipment[c] === equipName) {
+                alert("Equipment already included.");
+                return;
+            } 
+        }
+        this.equipment.push(equipName);
+    }
+
+    removeEquip(piece: string) {
+        let index = this.equipment.indexOf(piece);
+        this.equipment.splice(index, 1);
+    }
+
+    editEquip(piece: string) {
+        this.pieceBeingEdited = piece;
+    }
+
+    saveEquip(name: string, piece: string) {
+        this.equipment[this.equipment.indexOf(piece)] = name;
+        this.pieceBeingEdited = null;
+    }
 }
